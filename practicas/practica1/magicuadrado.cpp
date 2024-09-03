9ea388e3f08e71d89fd197bba1a4a1883fc5cb47
carriedoeliseo <carriedo.eliseo@gmail.com>
2024-09-03 16:01:11 -0300

    Ordeno carpetas, separo practica 1 en cpp independientes

--- practicas/practica1/magicuadrado.cpp
+++ practicas/practica1/magicuadrado.cpp
@@ -3,41 +3,6 @@
 
 using namespace std;
 
-// 1 ====================================================================================================
-
-void sumaSubconjuntos_recursivo (int i, int rest, vector<int>& conj, vector<vector<int>>& sol, vector<int> C) {
-    if (rest == 0) {
-        sol.push_back(conj);
-
-    } else if (rest < 0) {
-        return;
-
-    } else if (i == -1) {
-        return;
-
-    } else {
-        conj[i] = 1;
-        sumaSubconjuntos_recursivo(i-1, rest-C[i], conj, sol, C);
-        conj[i] = 0;
-        sumaSubconjuntos_recursivo(i-1, rest, conj, sol, C);
-
-    }
-}
-
-vector<vector<int>> sumaSubconjuntos (vector<int> C, int k) {
-    vector<int> conj (C.size());
-    for (int i = 0; i < conj.size(); i++) {
-        conj[i] = 0;
-
-    }
-    vector<vector<int>> sol = {};
-    sumaSubconjuntos_recursivo(C.size()-1, k, conj, sol, C);
-    return sol;
-
-}
-
-// 2 ====================================================================================================
-
 int esValido (vector<vector<int>> matriz) {
     int magic_number = 0;
     bool val = true;
@@ -174,115 +139,23 @@ int magicuadrado_podado (int n) {
 
 }
 
-// 3 ====================================================================================================
+int main () {
+    cout << "(1- magicuadrado)" << " " << "(2- magicuadrado podado)" << endl;
+    int opt_mc;
+    cin >> opt_mc;
 
-void maxisubconjunto_recursivo (vector<vector<int>> M, vector<int>& conj, int k, vector<int>& sol, int& max_sum, int n) {
-    if (conj.size() == k) {
-        int suma = 0;
-        for (int fil = 0; fil < k; fil++) {
-            for (int col = 0; col < k; col++) {
-                suma = suma + M[conj[fil]][conj[col]];
+    if (opt_mc == 1) {
+        int n;
+        cout << "n: " ; cin >> n;
+        int res = magicuadrado (n);
+        cout << res;
 
-            }
-        }
-        if (suma >= max_sum) {
-            sol = conj;
-            max_sum = suma;
-
-        }
-
-    } else if (M.size()-n > k-conj.size()) {
-        maxisubconjunto_recursivo(M, conj, k, sol, max_sum, n+1);
-        conj.push_back(n);
-        maxisubconjunto_recursivo(M, conj, k, sol, max_sum, n+1);
-        conj.pop_back();
-
-    } else {
-        conj.push_back(n);
-        maxisubconjunto_recursivo(M, conj, k, sol, max_sum, n+1);
-        conj.pop_back();
+    } else if (opt_mc == 2) {
+        int n;
+        cout << "n: " ; cin >> n;
+        int res = magicuadrado_podado (n);
+        cout << res;
 
     }
-}
-
-vector<int> maxisubconjunto (vector<vector<int>> M, int k) {
-    vector<int> max_conj = {};
-    vector<int> conj = {};
-    int max_sum = 0;
-    maxisubconjunto_recursivo(M, conj, k, max_conj, max_sum, 0);
-    return max_conj;
-
-}
-
-// main ====================================================================================================
-
-int main() {
-    cout << "(1- sumaSubconjuntos)" << "(2- magicuadrado)" << "(3- maxisubconjunto)" << endl;
-    int opt;
-    cin >> opt;
-
-    if (opt == 1) {
-        int n;
-        cout << "n: "; cin >> n ;
-        vector<int> C (n);
-        cout << "C: " ;
-        for (int i = 0; i < n; i++) {
-            cin >> C[i];
-
-        }
-        int k;
-        cout << "k: " ; cin >> k ;
-        vector<vector<int>> res = sumaSubconjuntos(C, k);
-        for (int i = 0; i < res.size(); i ++) {
-            cout << "{ " ;
-            for (int j = 0; j < res[i].size(); j++) {
-                cout << res[i][j] << " " ;
-
-            }
-            cout << "};" << endl ;
-        }
-
-    } else if (opt == 2) {
-        cout << "(1- magicuadrado)" << " " << "(2- magicuadrado podado)" << endl;
-        int opt_mc;
-        cin >> opt_mc;
-
-        if (opt_mc == 1) {
-            int n;
-            cout << "n: " ; cin >> n;
-            int res = magicuadrado (n);
-            cout << res;
-
-        } else if (opt_mc == 2) {
-            int n;
-            cout << "n: " ; cin >> n;
-            int res = magicuadrado_podado (n);
-            cout << res;
-
-        }
-        
-    } else if (opt == 3) {
-        int n;
-        cout << "n: " ; cin >> n ;
-        vector<vector<int>> M (n, vector<int>(n));
-        cout << "M:" << endl ;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> M[i][j];
-            }
-            cout << endl ;
-        }
-        int k;
-        cout << "k: " ; cin >> k ;
-        vector<int> res = maxisubconjunto(M, k);
-        cout << "{ " ;
-        for (int val : res) {
-            cout << val << " ";
-
-        }
-        cout << "}" ;
-    }
-
     return 0;
 }
-
