5c07404cfe81c7d6b79ad54ae8c13e335192fc28
carriedoeliseo <carriedo.eliseo@gmail.com>
2024-09-16 17:52:57 -0300

    Hago bottom up

--- practicas/practica1/pd_corteseconomicos.cpp
+++ practicas/practica1/pd_corteseconomicos.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#define INF 1000000000
+#include <vector>
+#define INF 1000000000 //1e9
 using namespace std;
 
-int l;
-int cs;
+int l; // long inicial de la vara.
+int n; // # cortes.
+int ans;
 int C[101];
 int M[101][101][101];
 
@@ -23,11 +25,44 @@ int ce_top (int i, int j, int c) {
     }
 }
 
+int ce_bottom (vector<int> V) {
+    for (int i = n; i > 0; i--) {
+        int min = INF;
+        int c;
+        for (int k = 1; k <= i; k++) {
+            if (V[k+1]-V[k-1] < min) {
+                min = V[k+1]-V[k-1];
+                c = k;
+
+            }
+        }
+        ans = ans + min;
+        V.erase(V.begin() + c);
+    }
+    return ans;
+
+}
+
 int main() {
-    cin >> l >> cs;
+    cin >> l >> n;
+    vector<int> V (n+2);
+    V[0] = 0; V[n+1] = l;
+    for (int k = 1; k <= n; k++) {
+        int v; cin >> v;
+        V[k] = v;
+
+    }
+    cout << ce_bottom(V) << endl;
+    return 0;
+
+}
+
+/*
+int main() {
+    cin >> l >> n;
     C[0] = 0; 
-    C[cs+1] = l;
-    for (int k = 1; k <= cs; k++) {
+    C[n+1] = l;
+    for (int k = 1; k <= n; k++) {
         cin >> C[k];
 
     }
@@ -43,3 +78,4 @@ int main() {
     return 0;
 
 }
+*/
