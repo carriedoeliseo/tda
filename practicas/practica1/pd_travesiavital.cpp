dd137142db2c76b15cc128c6e754f999ba173647
carriedoeliseo <carriedo.eliseo@gmail.com>
2024-09-21 19:46:11 -0300

    Termino el bottom up, esta bien

--- practicas/practica1/pd_travesiavital.cpp
+++ practicas/practica1/pd_travesiavital.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int m, n;
 int A[100][100];
 int M[100][100];
-int S[2][100];
+int S[100];
 
 int tv_top (int i, int j) {
     if (M[i][j] != -1) {
@@ -28,47 +28,33 @@ int tv_top (int i, int j) {
 
 int tv_bottom () {
     if (m >= n) {
-        for (int j = 0; j < n-1; j++) {
-            S[1][j] = A[m-1][j];
+        S[n-1] = 1 - A[m-1][n-1];
+        for (int j = n-2; j >= 0; j--) {
+            S[j] = max(1, S[j+1] - A[m-1][j]);
 
         }
-        S[1][n-1] = 1 - A[m-1][n-1];
         for (int i = m-2; i >= 0; i--) {
-            S[0][n-1] = A[i][n-1];
-            S[1][n-1] = max(1, S[1][n-1] - S[0][n-1]);
-            for (int j = n-2; j > 0; j--) {
-                S[0][j] = A[i][j];
-                S[1][j-1] = max(1, S[1][j] - S[1][j-1]);
-                S[1][j] = max(1, min(S[1][j], S[1][j+1]) - S[0][j]);
+            S[n-1] = max(1, S[n-1] - A[i][n-1]);
+            for (int j = n-2; j >= 0; j--) {
+                S[j] = max(1, min(S[j], S[j+1]) - A[i][j]);
                 
-
             }
-            S[0][0] = A[i][0];
-            S[1][0] = max(1, min(S[1][0], S[1][1]) - S[0][0]);
-
         }
     } else {
-        for (int i = 0; i < m-1; i++) {
-            S[1][i] = A[i][n-1];
+        S[m-1] = 1 - A[m-1][n-1];
+        for (int i = m-2; i >= 0; i--) {
+            S[i] = max(1, S[i+1] - A[n-1][i]);
 
         }
-        S[1][m-1] = 1 - A[m-1][n-1];
         for (int j = n-2; j >= 0; j--) {
-            S[0][m-1] = A[m-1][j];
-            S[1][m-2] = max(1, S[1][m-1] - S[1][m-2]);
-            S[1][m-1] = max(1, S[1][m-1] - S[0][m-1]);
-            for (int i = m-2; i > 0; i--) {
-                S[0][i] = A[i][j];
-                S[1][i-1] = max(1, S[1][i] - S[1][i-1]);
-                S[1][i] = max(1, min(S[1][i], S[1][i+1]) - S[0][i]);
-
+            S[m-1] = max(1, S[m-1] - A[m-1][j]);
+            for (int i = m-2; i >= 0; i--) {
+                S[i] = max(1, min(S[i], S[i+1]) - A[i][j]);
+                
             }
-            S[0][0] = A[0][j];
-            S[1][0] = max(1, min(S[1][0], S[1][1]) - S[0][0]);
-
         }
     }
-    return S[1][0];
+    return S[0];
 
 }
 
