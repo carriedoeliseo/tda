f0d8ffe71b9ba15ecaa16f2f2aaaa57d912e5bc7
carriedoeliseo <carriedo.eliseo@gmail.com>
2024-09-23 23:44:41 -0300

    Arreglo Ejercicio 15

--- practicas/practica1/gr_sumagolosa.cpp
+++ practicas/practica1/gr_sumagolosa.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 int sumagolosa (vector<int> G) {
     int ans = 0;
-    sort(G.begin(), G.end());
     for (int i = 0; i < N-1; i++) {
+        sort(G.begin()+i, G.end());
         ans = ans + G[i] + G[i+1];
         G[i+1] = G[i] + G[i+1];
 
