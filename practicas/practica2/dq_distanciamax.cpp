e734edcbb9413727a78bf482fa65c09ef214911a
carriedoeliseo <carriedo.eliseo@gmail.com>
2024-09-24 21:26:37 -0300

    Ejercicio 6, incompleto

---
+++ practicas/practica2/dq_distanciamax.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+int V;
+using namespace std;
+
+
+
+tuple<int, int> distanciamax (vector<vector<int>>& E) {
+
+
+}
+
+int main () {
+    cin >> V;
+    vector<vector<int>> E (V+1, vector<int>(2));
+    for (int i = 1; i < V+1; i++) cin << E[i][0] << E[i][1];
+    
+}
